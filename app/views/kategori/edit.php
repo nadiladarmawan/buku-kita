8ce17dec6692e284248ae8bd4dbed3bdbdc2818b
nadiladarmawan <nadila2022.tif@studens.poltek-kampar.ac.id>
2024-05-08 15:18:35 +0700

    update

---
+++ app/views/kategori/edit.php
@@ -0,0 +1,53 @@
+code create.php untuk kategori
+
+<!-- Content Wrapper. Contains page content --> 
+<div class="content-wrapper"> 
+    <!-- Content Header (Page header) --> 
+    <section class="content-header"> 
+        <div class="container-fluid"> 
+            <div class="row mb-2"> 
+                <div class="col-sm-6"> 
+                    <h1>Halaman Kategori</h1> 
+                </div> 
+            </div> 
+        </div><!-- /.container-fluid --> 
+    </section> 
+ 
+    <!-- Main content --> 
+    <section class="content"> 
+        <!--  --> 
+        <!-- Default box --> 
+ 
+        <div class="card"> 
+            <div class="card-header"> 
+                <h3 class="card-title"><?= $data['title'] ?></h3>  
+            </div> 
+            <div class="card-body"> 
+                <!-- code untuk form tambah --> 
+                    <div class="row">
+                        <div class="col-sm-3"></div>
+                        <div class="col-sm-6">
+                            <form action="<?=base_url?>kategori/updatekategori" method="POST">
+                                <div class="form-group">
+                                    <label for="">Nama Kategori</label>
+                                    <input type="hidden" name="id" value="<?=$data['kategori']['id']?>">
+                                    <input type="text" name="nama_kategori" class="form-control" value="<?=$data['kategori']['nama_kategori']?>">
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                <!-- end code form tambah --> 
+            </div> 
+            <!-- /.card-body --> 
+            <div class="card-footer"> 
+                Footer 
+            </div> 
+            <!-- /.card-footer--> 
+        </div> 
+        <!-- /.card --> 
+ 
+    </section> 
+    <!-- /.content --> 
+</div> 
+<!-- /.content-wrapper -->
---
+++ index.html
@@ -0,0 +1,5 @@
+<!-- /.navbar -->
+<!-- Main Sidebar Container -->
+<!-- Main Sidebar Container -->
+
+<!-- Content Wrapper. Contains page content -->
